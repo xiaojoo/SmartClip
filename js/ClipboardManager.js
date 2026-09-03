287b9f3ca5a514510f90ffb8815c0d0c4d38ace2
xiaojoo <hxttxiao@gmail.com>
2026-09-03 23:22:32 +0800

    Fix tree: ClipboardManager.js must not be .pragma library (needs clipboardStore context property)

--- js/ClipboardManager.js
+++ js/ClipboardManager.js
@@ -1,6 +1,6 @@
-.pragma library
-
 // ---- thin wrapper around the clipboardStore context property ----
+// NOTE: this is a normal (non-library) JS module so it can see the
+// clipboardStore context property of the QML scope that imports it.
 
 function items(query) {
     return clipboardStore.items(query || "")
